4d4c01415e2ef57b2cd49a0f972fe5938d4b7eac
meghamkpatel <32498703+meghamkpatel@users.noreply.github.com>
2022-12-06 16:25:29 -0500

    initialized Counselling and Health

--- HomelessShelter/src/Backend/Enterprise/Health.java
+++ HomelessShelter/src/Backend/Enterprise/Health.java
@@ -3,20 +3,64 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Backend.Enterprise;
+import Backend.Organization.Counselling;
+import Backend.Organization.Medical;
 import Backend.Role.Role;
 import java.util.ArrayList;
+import java.util.List;
 /**
  *
  * @author Sahil
  */
-public class Health {
+public class Health extends Enterprise{
      
-    private String health;
+    //private String health;
+    List<Counselling> rehabs;
+    List<Medical> hospitals;
     
-    public Health(String name) {
-        super(name);
-        health = name;
+    public Health(String name, EnterpriseType type) {
+        super(name, type);
+        //health = name;
+        rehabs = new ArrayList<>();
+        hospitals = new ArrayList<>();
     }
+
+    public List<Counselling> getRehabs() {
+        return rehabs;
+    }
+
+    public void setRehabs(List<Counselling> rehabs) {
+        this.rehabs = rehabs;
+    }
+
+    public List<Medical> getHospitals() {
+        return hospitals;
+    }
+
+    public void setHospitals(List<Medical> hospitals) {
+        this.hospitals = hospitals;
+    }
+    
+    public List<Counselling> addRehab(Counselling c){
+        rehabs.add(c);
+        return rehabs;
+    }
+    
+    public List<Counselling> deleteRehab(Counselling c){
+        rehabs.remove(c);
+        return rehabs;
+    }
+    
+    public List<Medical> addHospital(Medical c){
+        hospitals.add(c);
+        return hospitals;
+    }
+    
+    public List<Medical> deleteHospital(Medical c){
+        hospitals.remove(c);
+        return hospitals;
+    }
+        
 
     @Override
     public ArrayList<Role> getSupportedRole() {
--- HomelessShelter/src/Backend/Organization/Counselling.java
+++ HomelessShelter/src/Backend/Organization/Counselling.java
@@ -4,10 +4,64 @@
  */
 package Backend.Organization;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Sahil
  */
-public class Counselling {
+public abstract class Counselling extends Organization{
+    List<String> specialties;
+    double fees;
+    boolean acceptsMedicare;
+    boolean addiction;
+    boolean mentalHealth;
+    
+    public Counselling(String name) {
+        super(name);
+        specialties = new ArrayList<>();
+    }
+
+    public List<String> getSpecialties() {
+        return specialties;
+    }
+
+    public void setSpecialties(List<String> specialties) {
+        this.specialties = specialties;
+    }
+
+    public double getFees() {
+        return fees;
+    }
+
+    public void setFees(double fees) {
+        this.fees = fees;
+    }
+
+    public boolean isAcceptsMedicare() {
+        return acceptsMedicare;
+    }
+
+    public void setAcceptsMedicare(boolean acceptsMedicare) {
+        this.acceptsMedicare = acceptsMedicare;
+    }
+
+    public boolean isAddiction() {
+        return addiction;
+    }
+
+    public void setAddiction(boolean addiction) {
+        this.addiction = addiction;
+    }
+
+    public boolean isMentalHealth() {
+        return mentalHealth;
+    }
+
+    public void setMentalHealth(boolean mentalHealth) {
+        this.mentalHealth = mentalHealth;
+    }
+    
     
 }
