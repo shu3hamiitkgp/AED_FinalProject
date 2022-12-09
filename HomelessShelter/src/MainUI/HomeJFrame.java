4cdb2d2a02199fa66b2ea4803564ab8fa35e4faf
Sahil Gothoskar <33109078+SahilGothoskar@users.noreply.github.com>
2022-12-09 15:57:55 -0500

    MedicalViewUI

--- HomelessShelter/src/MainUI/HomeJFrame.java
+++ HomelessShelter/src/MainUI/HomeJFrame.java
@@ -34,6 +34,11 @@ public class HomeJFrame extends javax.swing.JFrame {
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jButton1.setText("Resources");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -80,6 +85,10 @@ public class HomeJFrame extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
