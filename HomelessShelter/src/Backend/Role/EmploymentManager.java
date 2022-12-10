bde9e98fe7cec0457b0e9e3a50844e1c6c195565
Sahil Gothoskar <33109078+SahilGothoskar@users.noreply.github.com>
2022-12-10 18:38:54 -0500

    UI + Roles

--- HomelessShelter/src/Backend/Role/EmploymentManager.java
+++ HomelessShelter/src/Backend/Role/EmploymentManager.java
@@ -4,10 +4,37 @@
  */
 package Backend.Role;
 
+import Backend.Ecosystem.EcoSystem;
+import Backend.Enterprise.Enterprise;
+import Backend.Network.Network;
+import Backend.Organization.Jobs;
+import Backend.Organization.Organization;
+import Backend.UserAccount.UserAccount;
+import javax.swing.JPanel;
+
 /**
  *
  * @author Sahil
  */
-public class EmploymentManager {
+public class EmploymentManager extends Role{
+
+    
+        Jobs jobs;
+
+    public Jobs getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(Jobs jobs) {
+        this.jobs = jobs;
+    }
+    
+    
+    
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, Network network, EcoSystem business) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+         return new EmploymentManagerAreaJPanel(userProcessContainer, account, organization, enterprise, network, business);
+    }
     
 }
